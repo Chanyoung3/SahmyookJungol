2297d39c69607b36dcfecff720ee3f1695f9863a
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-30 17:12:26 +0900

    Initial commit

---
+++ Jungol/src/q528/Main.java
@@ -0,0 +1,17 @@
+package q528;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+        if( n <0 ){
+            System.out.println(n);
+            System.out.println("minus");
+        }
+        else { System.out.println(n); }
+    }
+}
