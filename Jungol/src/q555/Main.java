39ea36796b014d538c5d1632b8b87fd78ad94d98
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-31 11:07:49 +0900

    Initicial commit

--- Jungol/src/q555/Main.java
+++ Jungol/src/q555/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        String[] array = new String[10];
+        char[] array = new char[10];
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 10 ; i++){
-            array[i] = sc.next();
+            array[i] = sc.next().charAt(0);
         }
        sc.close();
        
