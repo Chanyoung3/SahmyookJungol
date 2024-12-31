829ada45b69768d2e1b378996774af41b837aa24
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-31 17:22:27 +0900

    Initial commit

--- Jungol/src/q614/Main.java
+++ Jungol/src/q614/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Student student = new Student();
+		Student student1 = new Student();
 		Scanner sc = new Scanner(System.in);
 		String a = sc.next();
 		int b = sc.nextInt();
 		sc.close();
 		
-		student.setInfo(a, b);
-		student.print();
+		student1.setInfo(a, b);
+		student1.print();
 	}
 }
