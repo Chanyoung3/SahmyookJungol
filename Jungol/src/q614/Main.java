d7b98410ff6d4f30c7c6517ae5ca630f5221ae55
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-31 17:26:26 +0900

    Initial commit

--- Jungol/src/q614/Main.java
+++ Jungol/src/q614/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Student student1 = new Student();
 		Scanner sc = new Scanner(System.in);
 		String a = sc.next();
 		int b = sc.nextInt();
 		sc.close();
 		
-		student1.setInfo(a, b);
+		Student student1 = new Student();
 		student1.print();
+		Student student2 = new Student();
+		student2.setInfo(a, b);
+		student2.print();
 	}
 }
--- Jungol/src/q614/Student.java
+++ Jungol/src/q614/Student.java
@@ -4,10 +4,6 @@ class Student {
 	private String school = "Jejuelementary";
 	private int grade = 6;
 	
-	public Student() {
-		System.out.println(grade + " grade in " + school + " School");
-	}
-	
 	public void setInfo(String school, int grade) {
 		this.school = school;
 		this.grade = grade;
