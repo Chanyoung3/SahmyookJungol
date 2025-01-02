c4c409c6061674adbbbedd7a4046c3e166f3c2cb
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-02 10:17:20 +0900

    Initial Commit

--- Jungol/src/q571/Main.java
+++ Jungol/src/q571/Main.java
@@ -3,6 +3,12 @@ package q571;
 import java.util.Scanner;
 
 public class Main {
+	public static void print(int inum) {
+		for(int i = 0; i < inum; i++) {
+			System.out.println("~!@#$^&*()_+|");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int inum = sc.nextInt();
@@ -10,10 +16,5 @@ public class Main {
 		
 		
 		sc.close();
-	}
-	static void print(int inum) {
-		for(int i = 0; i < inum; i++) {
-			System.out.println("~!@#$^&*()_+|");
-		}
 	}
 }
