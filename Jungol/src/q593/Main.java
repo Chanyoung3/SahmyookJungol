b04507a9c59883a9af1f4b2dc99ab47844fba1d1
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-03 09:11:46 +0900

    Initial Commit

---
+++ Jungol/src/q593/Main.java
@@ -0,0 +1,20 @@
+package q593;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print("ASCII code =? ");
+			int inum = sc.nextInt();
+		
+			if(inum >= 33 && inum <= 127) {
+				char ch = (char) inum;
+				System.out.println(ch);
+			}
+			else break;
+		}
+	}
+}
