65b988647d9a23ac97e06ca41cdcce2e8e02fd3f
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-03 15:42:05 +0900

    Initial Commit

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,13 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String sh = "My height";
+		int ih = 170;
+		String sw = "My weight";
+		double dw = 68.6;
+		
+		System.out.printf(sh + "\n" + ih + "\n" + sw  + "\n%.6f", dw);
+	}
+}
